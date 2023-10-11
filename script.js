2f87ca177ea225ef319a08d3c777c152a0dfc54c
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2023-10-11 13:18:52 +0530

    Fixed issue - Add contact should add 1 entry only.

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ container.innerHTML = `
           <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
         <div class="toast-body bg-success text-white">
-          Your opinion is recorded successfully.
+          Contact Saved Successfully :-)
         </div>
       </div>
     </div>
@@ -117,8 +117,8 @@ document.body.append(container);
 let contactEntries = document.getElementById("contactEntries");
 
 window.onload = (event) => {
-    document.getElementById("tableContainer").style.display = "block";
-    getData();
+  document.getElementById("tableContainer").style.display = "block";
+  getData();
 };
 
 // Target Form Element
@@ -127,91 +127,98 @@ const contactForm = document.getElementById("form");
 function formSubmit() {
 
 
-    // Activate Bootstrap Validations
-    contactForm.classList.add("was-validated");
+  // Activate Bootstrap Validations
+  contactForm.classList.add("was-validated");
 
-    // Create Bootstrap Toast Trigger
-    let invalidElement = document.getElementById("invalidToast");
-    let invalidToast = new bootstrap.Toast(invalidElement, {
-        delay: 3000
+  // Create Bootstrap Toast Trigger
+  let invalidElement = document.getElementById("invalidToast");
+  let invalidToast = new bootstrap.Toast(invalidElement, {
+    delay: 3000
+  });
+
+  // Get Contact Form Input Data    
+  const formData = new FormData(contactForm);
+  const formDataObj = Object.fromEntries(formData.entries());
+
+  // Check for invalid form Values
+  if (!contactForm.checkValidity()) {
+    document.getElementById("checkboxOptions").style.display = "none";
+    invalidToast.show(); // Activate Invalid Toast (Error Message)
+    return;
+  }
+
+  // Success Scenario
+  else {
+    document.getElementById("tableContainer").style.display = "block";
+
+    addData(formDataObj); // Add Form data to Table
+
+    let validElement = document.getElementById("validToast");
+    let validToast = new bootstrap.Toast(validElement, {
+      delay: 3000
     });
-
-    // Get Contact Form Input Data    
-    const formData = new FormData(contactForm);
-    const formDataObj = Object.fromEntries(formData.entries());
-
-    // Check for invalid form Values
-    if (!contactForm.checkValidity()) {
-        document.getElementById("checkboxOptions").style.display = "none";
-        invalidToast.show(); // Activate Invalid Toast (Error Message)
-        return;
-    }
-
-    // Success Scenario
-    else {
-        document.getElementById("tableContainer").style.display = "block";
-
-        addData(formDataObj); // Add Form data to Table
-        let validElement = document.getElementById("validToast");
-        let validToast = new bootstrap.Toast(validElement, {
-            delay: 3000
-        });
-        validToast.show(); // Activate Valid Toast (Success Message)
-        contactForm.reset(); // Reset Form
-        contactForm.classList.remove("was-validated"); // Remove Bootstrap Validation CSS
-        document.getElementById("tableContainer").scrollIntoView(); // Move to Table
-    }
+    validToast.show(); // Activate Valid Toast (Success Message)
+    contactForm.reset(); // Reset Form
+    contactForm.classList.remove("was-validated"); // Remove Bootstrap Validation CSS
+    document.getElementById("tableContainer").scrollIntoView(); // Move to Table
+  }
 };
 
 
 // Function for adding Contact Form Data to Table
 const addData = ((dataObj) => {
-    console.log(dataObj);
-    window.localStorage.setItem(dataObj.name, dataObj.number);
-    getData();
+  console.log(dataObj);
+  window.localStorage.setItem(dataObj.name, dataObj.number);
+  contactEntries.innerHTML += `
+    <tr>
+      <td>${dataObj.name}</td>
+      <td>${dataObj.number}</td>
+      <td><button class="btn" id="editBtn" onClick="editContact()"><i class="fa fa-pencil-square" aria-hidden="true"></i></button> &nbsp; &nbsp;
+      <button class="btn" id="deleteBtn" onClick="deleteContactAlert('${number}')"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
+    </tr>`;
 });
 
 // Function to get data from Local Storage
 const getData = () => {
-    for (let i = 0; i < localStorage.length; i++) {
-        let number = localStorage.key(i);
-        let name = localStorage.getItem(number);
-        contactEntries.innerHTML += `
-        <tr>
-            <td>${name}</td>
-            <td>${number}</td>
-            <td><button class="btn" id="editBtn" onClick="editContact()"><i class="fa fa-pencil-square" aria-hidden="true"></i></button> &nbsp; &nbsp;
-            <button class="btn" id="deleteBtn" onClick="deleteContactAlert('${number}')"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
-        </tr>`;
-    }
+  for (let i = 0; i < localStorage.length; i++) {
+    let number = localStorage.key(i);
+    let name = localStorage.getItem(number);
+    contactEntries.innerHTML += `
+      <tr>
+        <td>${name}</td>
+          <td>${number}</td>
+          <td><button class="btn" id="editBtn" onClick="editContact()"><i class="fa fa-pencil-square" aria-hidden="true"></i></button> &nbsp; &nbsp;
+          <button class="btn" id="deleteBtn" onClick="deleteContactAlert('${number}')"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
+      </tr>`;
+  }
 }
 
 
 function editContact() {
-    console.log("Edit Function");
+  console.log("Edit Function");
 }
 
 // Created a letiable to pass between two functions
 let numberToBeDeleted;
 
 function deleteContactAlert(value) {
-    console.log('Value', value);
-    let deleteElement = document.getElementById("deleteToast");
-    let deleteToast = new bootstrap.Toast(deleteElement, {
-        delay: 10000
-    });
-    deleteToast.show();
+  console.log('Value', value);
+  let deleteElement = document.getElementById("deleteToast");
+  let deleteToast = new bootstrap.Toast(deleteElement, {
+    delay: 10000
+  });
+  deleteToast.show();
 
-    numberToBeDeleted = value;
+  numberToBeDeleted = value;
 }
 
 
 function deleteContact() {
-    window.localStorage.removeItem(numberToBeDeleted);
-    location.reload(); // Reloading to get the updated Data after Deletion.
+  window.localStorage.removeItem(numberToBeDeleted);
+  location.reload(); // Reloading to get the updated Data after Deletion.
 
 }
 
 function reset() {
-    contactForm.reset();
+  contactForm.reset();
 }
