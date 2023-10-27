a18999018d1d20407c87417a0654e2c0a06ab617
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2023-10-27 10:47:41 +0530

    Remove h1 ending tag (Unused)

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ container.innerHTML = `
     <div class="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true" >
       <div class="modal-dialog">
         <div class="modal-content">
-          <div class="modal-header">Edit Contact</h1>
+          <div class="modal-header">Edit Contact
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
